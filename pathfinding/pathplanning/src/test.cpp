643faf2b982ffb785b91bdea5c4012316b3eff9d
mabuc13 <mabuc13@student.sdu.dk>
2017-04-05 13:58:56 +0200

    Temporary pathplanning where markerlocator is to be implemented

---
+++ pathfinding/pathplanning/src/test.cpp
@@ -0,0 +1,19 @@
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sensor_msgs/image_encodings.h>
+
+//#include "markerlocator/markerpose.h"
+
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+
+//#include <sensor_msgs/Image>
+using namespace std;
+using namespace cv;
+
+
+int main()
+{
+return 0;
+}
