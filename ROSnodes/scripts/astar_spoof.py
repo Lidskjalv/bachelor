60ebf11d48de92c2f6775f9c0b5e529ef0310b08
t-g-j <t.g.j@sol.dk>
2017-05-19 08:27:13 +0200

    new spoofing node

--- ROSnodes/scripts/astar_spoof.py
+++ ROSnodes/scripts/astar_spoof.py
@@ -1,3 +1,32 @@
+#!/usr/bin/env python
+"""OpenCV feature detectors with ros Image Topics in python.
+
+This example subscribes to a ros topic containing sensor_msgs
+Image. It converts the Image into a numpy.ndarray,
+then detects and marks features in that image. It finally displays
+and publishes the new image - again as Image topic.
+"""
+__author__ =  'Thor Jensen'
+__version__=  '0.1'
+
+# Python libs
+import sys, time
+
+# numpy and scipy
+import numpy as np
+from scipy.ndimage import filters
+
+# OpenCV
+import cv2
+# Ros libraries
+import roslib
+import rospy
+
+# Ros Messages
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+
 class image_feature:
 
     def __init__(self):
@@ -5,7 +34,8 @@ class image_feature:
         # topic where we publish
         self.image_pub = rospy.Publisher("/waypoints",
             Image,queue_size = 1)
-
+        self.image_pub = rospy.Subscriber("/output/image_treated",
+            Image,queue_size = 1)
         #self.image_pub_display = rospy.Publisher("/display_image",Image,queue_size =1)
         # self.bridge = CvBridge()
 
